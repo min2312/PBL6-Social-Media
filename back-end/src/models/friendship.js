c9d818d3910313c8eb37a318f1e7b17776dbc216
Nguyễn Quang Minh <quangminh23122004@gmail.com>
2025-10-26 01:09:52 +0700

    Handle API socket,social,friends

--- back-end/src/models/friendship.js
+++ back-end/src/models/friendship.js
@@ -4,8 +4,16 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
 	class Friendship extends Model {
 		static associate(models) {
-			Friendship.belongsTo(models.User, { foreignKey: "userId" });
-			Friendship.belongsTo(models.User, { foreignKey: "friendId" });
+			// người gửi
+			Friendship.belongsTo(models.User, {
+				foreignKey: "userId",
+				as: "Sender",
+			});
+			// người nhận
+			Friendship.belongsTo(models.User, {
+				foreignKey: "friendId",
+				as: "Receiver",
+			});
 		}
 	}
 	Friendship.init(
